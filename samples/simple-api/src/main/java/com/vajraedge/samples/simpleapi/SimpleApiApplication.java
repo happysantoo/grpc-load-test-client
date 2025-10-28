d7b5ecede6e34105fb3df194fa65949ff3165c52
happysantoo <santoo.k@gmail.com>
2025-10-28 09:54:52 -0400

    simple test changes and blog

--- samples/simple-api/src/main/java/com/vajraedge/samples/simpleapi/SimpleApiApplication.java
+++ samples/simple-api/src/main/java/com/vajraedge/samples/simpleapi/SimpleApiApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleApiApplication {
 
+ 
+
     public static void main(String[] args) {
         SpringApplication.run(SimpleApiApplication.class, args);
     }
